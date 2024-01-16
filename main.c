ed7af63127a24bf3d1fb56f023841cefd97a8b24
Kofiad <kofiadaba@gmail.com>
2024-01-16 22:55:32 +0000

    working on main file

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "monty.h"
+
+/**
+ * main - Entry point for the Monty bytecode interpreter
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: EXIT_SUCCESS on success, or EXIT_FAILURE on failure
+ */
+int main(int argc, char *argv[])
+{
+	/* Your code here */
+
+	return (EXIT_SUCCESS);
+}
