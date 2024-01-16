1413004c8b6b9852a62ffdc1e193d7b0e1addf9a
Kofiad <kofiadaba@gmail.com>
2024-01-16 22:25:15 +0000

    trying to compile

--- monty.h
+++ monty.h
@@ -42,6 +42,7 @@ void pall(stack_t **stack);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
 /*Helper function prototypes*/
 int _isnumber(char *s);
