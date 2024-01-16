efc2f1ef9c59e8501dac2d3f1757400eddcea37a
Kofiad <kofiadaba@gmail.com>
2024-01-16 10:48:01 +0000

    Added partial header file

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
